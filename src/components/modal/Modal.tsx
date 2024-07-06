cbc6939e6f8a417928fd0f10c268585d18cde34e
Igor Siamykin <sparkisisss@gmail.com>
2024-07-06 20:08:48 +0300

    feat: a form validation and change some style

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,29 +1,45 @@
-import ReactDOM from 'react-dom'; 
-import classes from './Modal.module.css'  
+import ReactDOM from 'react-dom';
+import { useForm } from 'react-hook-form';
+import classes from './Modal.module.css'; 
 
 const portal = document.getElementById('portal') as HTMLElement
 
 const Modal = () => {
+    const {register, handleSubmit, reset, formState: {errors, isValid}} = useForm({
+        defaultValues: {
+            email: "",
+            password: ''
+        },
+        mode: "onBlur"
+    })
+
+    const onSubmit = (data:any):void => {
+        alert(JSON.stringify(data))
+        reset()
+    }
 
     return ReactDOM.createPortal(
         <section>
             <div className={classes.formBox}>
                 <div className={classes.formValue}>
-                    <form action="">
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <h2>Login</h2>
                         <div className={classes.inputbox}>
-                            <input type="email" required/>
-                            <label htmlFor="">Email</label>
-                        </div>
+                            <input type="email" required placeholder='Email'/>                       
+                        </div>                        
                         <div className={classes.inputbox}>
-                            <input type="password" required/>
-                            <label htmlFor="">Password</label>
+                            <input {...register("password", {required: "This is required", 
+                            minLength: {
+                                value: 5,
+                                message: "Min length is 5"
+                            }})} 
+                            type="password" required placeholder='Password'/>
+                            <p>{errors.password?.message}</p>
                         </div>
                         <div className={classes.forget}>
-                            <label htmlFor=""><input type='checkbox'/>Remember<a href="#">Forget password</a></label>
-                            
+                            <label htmlFor=""><input type='checkbox'/>Remember<a href="#">Forget password</a></label>                            
                         </div>
-                        <button>Log in</button>
+                        <button disabled={!isValid}>Log in</button>
                         <div className={classes.register}> 
                             <p>Don't have a acount <a href="#">Register</a></p>
                         </div>
