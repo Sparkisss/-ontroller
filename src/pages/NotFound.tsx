c89f07905175928cd5c5f89467a306792105455d
Igor Siamykin <sparkisisss@gmail.com>
2024-07-11 21:46:33 +0300

    refactor: change NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
 import { Link } from "react-router-dom"
+import errorPage from "../source/img/404-error.png"
+import Button from "../components/UI/button/Button";
 
 const NotFound: FC = () => {
     return (
-        <div>
-            404 Not Found
-            <Link to="/">Auth page</Link>
+        <div className="errorPage">
+            <img src={errorPage} alt="oh yet error" />
+            <Button>
+                <Link to="/">Back to auth page</Link>
+            </Button>            
         </div>
     );
 };
